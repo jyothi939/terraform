b93d42dd2893affc96492bc36507a91f1e9aa8d5
jyothi939 <166134576+jyothi939@users.noreply.github.com>
2025-09-11 20:27:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,8 +13,8 @@ provider "azurerm" {
   features {}
 
   subscription_id = "c8e4642a-2243-418a-b847-6ad9dea6f4e7"
-  client_id       = "78dd71de-8cab-4ca5-83f4-4e0dcd3ab8a6"
-  client_secret   = "oUJ8Q~_XwApAbddKHWxYsAaH.MLoTBQ82xM24biS"
+  client_id       = "464baaa4-c256-4206-90f2-31aa6bd34ebf"
+  client_secret   = "saO8Q~VfY.RhgLvaQSjRL25uqxy6yW2T-U2qUaag"
   tenant_id       = "cbead578-ee60-450b-87d5-8a1499324251"
 }
 
